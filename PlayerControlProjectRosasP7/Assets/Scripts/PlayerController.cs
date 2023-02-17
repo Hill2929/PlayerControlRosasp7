b3227be838965511973be0f8f35cae786a2b086a
Hill2929 <736120@student.dusd.net>
2023-02-17 15:23:00 -0800

    Finished car games
    Imported Assets and fisnihed Unit 1

--- PlayerControlProjectRosasP7/Assets/Scripts/PlayerController.cs
+++ PlayerControlProjectRosasP7/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,21 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float fowardInput;
+
     void Update()
     {
-        // Move the vehicle foward 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+        // Moves the car foward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        // Rotateds the car based on horizonrtal input 
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+
     }
 }
